2fe4f9611e7eebb595efd73471a8a27f688077ac
Patrick Grossmann <patrickdgro@gmail.com>
2021-12-28 20:14:26 +0100

    feat: Warning if adding game double

--- src/whatdo/custom.py
+++ src/whatdo/custom.py
@@ -2,7 +2,7 @@
 
 import uuid
 
-from whatdo.util import load_games, save_games
+from whatdo.util import load_games, save_games, col
 
 cache = None
 file = "custom.json"
@@ -29,5 +29,20 @@ def add_custom():
     erg["name"] = input("Please enter a name:")
     erg["img_icon_url"] = input("You may enter a icon url for display purpose:")
     erg["img_logo_url"] = input("Additionally you can enter a logo url:")
+    for i in games().values():
+        if i["name"] == erg["name"]:
+            print("There seems to already exist a game with that name!")
+            print("Do you want to: "
+                    + col("\x1b[31m") + "o" + col("\x1b[m") + "verwrite, "
+                    + col("\x1b[31m") + "a" + col("\x1b[m") + "dd anyway or "
+                    + col("\x1b[31m") + "c" + col("\x1b[m") + "cancel?")
+            inp = input(">")
+            if inp == "o":
+                erg["appid"] = i["appid"]
+                games()[erg["appid"]] = erg
+                return
+            if inp == "c":
+                return
+            break
     erg["appid"]= str(uuid.uuid4())
     games()[erg["appid"]] = erg
